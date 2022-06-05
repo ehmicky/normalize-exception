48517ccf8207fc42d6f86c1696a7bafbec0f507d
ehmicky <ehmicky@gmail.com>
2022-06-05 15:55:45 +0200

    Add TypeScript description

--- src/main.d.ts
+++ src/main.d.ts
@@ -1,7 +1,46 @@
 /**
+ * Normalize exception/error.
+ *
+ * In JavaScript, one can `throw` any value including strings, objects (like
+ * `{ message: "..." }`) or even `undefined`. This normalizes any exception
+ * to an `Error` instance.
+ *
+ * It also fixes any missing or invalid error properties: `name`, `message`,
+ * `stack`, `cause`, `errors`.
  *
  * @example
  * ```js
+ * import normalizeException from 'normalize-exception'
+ *
+ * try {
+ *   throw null
+ * } catch (error) {
+ *   const normalizedError = normalizeException(error)
+ *   console.log(normalizedError) // Error: null
+ *   // Without `normalizeException()`, this would throw
+ *   console.log(normalizedError.name)
+ * }
+ *
+ * console.log(normalizeException('message')) // Error: message
+ *
+ * console.log(normalizeException({ name: 'TypeError', message: 'message' })) // TypeError: message
+ *
+ * const error = new TypeError('message')
+ * console.log(error.stack) // TypeError: message
+ *
+ * // `error.stack` is cached, so it does not update
+ * error.message += ' otherMessage'
+ * console.log(error.stack) // TypeError: message
+ *
+ * const normalizedError = normalizeException(error)
+ * console.log(normalizedError.stack) // TypeError: message otherMessage
+ *
+ * const error = new Error('message', { cause: 'innerError' })
+ * console.log(error.cause instanceof Error) // false
+ *
+ * const normalizedError = normalizeException(error)
+ * console.log(normalizedError.cause instanceof Error) // true
+ * console.log(normalizedError.cause) // Error: innerError
  * ```
  */
 export default function normalizeException(error: any): Error
