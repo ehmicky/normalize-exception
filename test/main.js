f7baa387210cb326b9548de666d2fdd7c179f42f
ehmicky <ehmicky@gmail.com>
2022-06-05 13:40:14 +0200

    Add test

--- test/main.js
+++ test/main.js
@@ -1,6 +1,10 @@
 import test from 'ava'
 import normalizeException from 'normalize-exception'
 
-test('Dummy test', (t) => {
-  t.is(typeof normalizeException, 'function')
+test('Normal errors are left as is', (t) => {
+  const error = new TypeError('test')
+  const errorString = error.toString()
+  const errorA = normalizeException(error)
+  t.true(errorA instanceof TypeError)
+  t.is(errorA.toString(), errorString)
 })
