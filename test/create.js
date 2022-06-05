86f86dd78eacbfc01a611020e42839e85dc1c575
ehmicky <ehmicky@gmail.com>
2022-06-05 13:52:22 +0200

    Add more tests

--- test/create.js
+++ test/create.js
@@ -2,6 +2,8 @@ import test from 'ava'
 import normalizeException from 'normalize-exception'
 import { each } from 'test-each'
 
+const { propertyIsEnumerable: isEnum } = Object.prototype
+
 each(
   [
     undefined,
@@ -37,4 +39,41 @@ test('Handle exceptions with invalid toString()', (t) => {
   t.true(error instanceof Error)
   t.is(error.message, 'test')
   t.true(error.stack.includes('toString'))
+})
+
+test('Plain-objects errors can have names', (t) => {
+  const name = 'test'
+  const error = normalizeException({ name })
+  t.is(error.name, name)
+  t.false(isEnum.call(error, 'name'))
+  t.true(error instanceof Error)
+})
+
+test('Plain-objects errors can have messages', (t) => {
+  const message = 'test'
+  const error = normalizeException({ message })
+  t.is(error.message, message)
+  t.false(isEnum.call(error, 'message'))
+})
+
+test('Plain-objects errors can have stacks', (t) => {
+  const message = 'test'
+  const stack = `Error: ${message}\n  at here`
+  const error = normalizeException({ message, stack })
+  t.is(error.stack, stack)
+  t.false(isEnum.call(error, 'stack'))
+})
+
+test('Plain-objects errors can have causes', (t) => {
+  const cause = new Error('test')
+  const error = normalizeException({ cause })
+  t.is(error.cause, cause)
+  t.false(isEnum.call(error, 'cause'))
+})
+
+test('Plain-objects errors can have aggregate errors', (t) => {
+  const errors = [new Error('test')]
+  const error = normalizeException({ errors })
+  t.deepEqual(error.errors, errors)
+  t.false(isEnum.call(error, 'errors'))
 })
